a54a59a42ee9758c05582e7a8d83d3d804b8cb5f
Jack <j.jobs1028@gmail.com>
2018-06-05 16:33:24 -0700

    apply Crossfilter to make a timewindow

--- js/graph.js
+++ js/graph.js
@@ -1,7 +1,43 @@
+//location, incorporationDate, year_cd, indust_cd, indust_nm
+/*
+var chartIndustryByCode = dc.barChart('#indType');
+
+d3.csv("data/data.csv", function(err, data){
+    var ndx = crossfilter(data),
+    industryDim = ndx.dimension(function(d){return d.indust_cd;}),
+    IndustryGroup = industryDim.group().reduceSum(function(d){ return 1});
+    
+    chartIndustryByCode
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .brushOn(false)
+        .xAxisLabel('Industry code')
+        .yAxisLabel('Amount')
+        .dimension(industryDim)
+        .barPadding(0.1)
+        .outerPadding(0.05)
+        .group(IndustryGroup);
+    
+    
+    chartIndustryByCode.render();
+});
+*/
+
 d3.csv("data/data.csv").then(function (data) {
    // if (err) throw err;
-    var indTypeChart = dc.rowChart("#indType");
-    var locTypeChart = dc.rowChart("#locType");
+    var indTypeChart = dc.barChart("#indType");
+    var locTypeChart = dc.barChart("#locType");
+ 
+
+    var formatDate = d3.time.format("%b %d, %Y");
+    var nestByDate = d3.nest()
+        .key(function (d) { return d3.time.day(d.date); });
+    data.forEach(function (d, i) {
+        d.date = parseDate(d.date);
+    });
+
+    
+ 
     var ndx = crossfilter(data);
     var all = ndx.groupAll();
     console.log(all);
@@ -9,16 +45,33 @@ d3.csv("data/data.csv").then(function (data) {
     var indTypeDim = ndx.dimension(function (d) { return d["indust_nm"]; })
     var locTypeDim = ndx.dimension(function (d) { return d["location"]; })
 
-    var indTypeGroup = indTypeDim.group();
-    var locTypeGroup = locTypeDim.group();
+
+
+    var indTypeGroup = indTypeDim.group().reduceSum(function (d) { return 1; });
+    indTypeGroup = getTops(indTypeGroup);
+    var locTypeGroup = locTypeDim.group().reduceSum(function (d) { return 1; });
+    locTypeGroup = getTops(locTypeGroup);
 
     indTypeChart
+        .x(d3.scaleBand())
+        .xUnits(dc.units.ordinal)
         .dimension(indTypeDim)
-        .group(indTypeGroup);
-
+        .group(indTypeGroup)
     locTypeChart
+        .x(d3.scaleBand())
+        .xUnits(dc.units.ordinal)
         .dimension(locTypeDim)
-        .group(locTypeGroup)
-        //.data(function(group){});
+        .group(locTypeGroup);//{return group.top(4)});
+    //     .elasticX(true)
+    //     .data(function(group){return group.top(3)});
+
     dc.renderAll();
 })
+
+function getTops(source_group) {
+    return {
+        all: function () {
+            return source_group.top(5);
+        }
+    };
+}
